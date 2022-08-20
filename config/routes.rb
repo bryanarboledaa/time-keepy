a2a4299d38259da0baa4be5d4eaad4be126bb8af
bryanarboledaa <jbdarboleda@gmail.com>
2022-08-20 14:32:31 +0800

    devise setup for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'user/sessions'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "static_pages#index"
+
+  #user
+  resources :users
+
 end
