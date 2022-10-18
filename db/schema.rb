7f8b6400d29e94dbfca67a3edcaa6ae7234af588
bryanarboledaa <jbdarboleda@gmail.com>
2022-10-18 21:00:38 +0800

    haxxx fixed issue with migration files

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_06_105241) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_18_123502) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "time_records", force: :cascade do |t|
+    t.integer "user_id"
     t.datetime "date"
-    t.time "time"
-    t.string "comment"
+    t.datetime "time"
+    t.string "log_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
-    t.integer "log_type"
-    t.integer "status"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,7 +31,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_06_105241) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
